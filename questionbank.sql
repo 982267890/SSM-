f13af62062aa2c522b3f5df93c00fc80b389e455
MaoJiann_Fang <982267890@qq.com>
2020-03-14 01:14:48 +0800

    fourth commit

--- questionbank.sql
+++ questionbank.sql
@@ -104,13 +104,7 @@ UNLOCK TABLES;
 -- Table structure for table `time`
 --
 
-DROP TABLE IF EXISTS `time`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `time` (
-  `t` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `time`
--- src/main/java/com/Bean/Contnet.java
+++ src/main/java/com/Bean/Contnet.java
@@ -1,4 +1,40 @@
 package com.Bean;
 
 public class Contnet {
+    private int questionId;
+    private String anwers;
+    private String username;
+
+    @Override
+    public String toString() {
+        return "Contnet{" +
+                "questionId=" + questionId +
+                ", anwers='" + anwers + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnwers() {
+        return anwers;
+    }
+
+    public void setAnwers(String anwers) {
+        this.anwers = anwers;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
